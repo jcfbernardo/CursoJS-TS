6ade630530bd4fe807f25ba17d7d483b7e58089d
jcfbernardo <joaoc.bernardo12@gmail.com>
2023-04-10 22:35:50 -0300

    Criando um gerador de codigo de cpf (versão porca)

---
+++ Aula 97 - Validador de CPF/main.js
@@ -0,0 +1,44 @@
+let valorTotal = [];
+
+function cpf(cpf) {
+    let cpfLimpo = cpf.replace(/\D+/g, '');
+    let cpfArray = Array.from(cpfLimpo);
+    return calculaCpf(cpfArray);
+}
+
+function calculaCpf(cpfArray) {
+
+    let valorTotal = cpfArray;
+    let valorAux = multiplicaDigitosCPF(cpfArray)
+
+    function multiplicaDigitosCPF(arr) {
+        let arrAux = [];
+        arr.reduce((ac, val, index) => {
+            arrAux.splice(index, 0, (Number(val) * ac));
+            ac = ac - 1;
+            return ac;
+        }, (arr.length + 1));
+        return arrAux;
+    }
+
+    function defineDigito(arr) {
+        let soma = arr.reduce((ac, val) => ac + val, 0);
+        let calculo = (11 - (soma % 11));
+        if (calculo > 9) {
+            return 0;
+        } else {
+            return calculo;
+        }
+    }
+
+    let newNumero = defineDigito(valorAux);
+    valorTotal.push(newNumero);
+
+    if (valorTotal.length < 11) {
+        return calculaCpf(valorTotal);
+    } else {
+        return valorTotal;
+    }
+}
+
+console.log((cpf('070.987.720')));
